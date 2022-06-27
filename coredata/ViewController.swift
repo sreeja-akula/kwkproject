c68cd65ccef69f42c2a79767fe5861ca844d908b
Sreeja Akula <sreeja.akula7@gmail.com>
2022-06-27 15:28:53 -0400

    Initial Commit

---
+++ coredata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coredata
+//
+//  Created by Admin on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
